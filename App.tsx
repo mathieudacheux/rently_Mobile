7ff796be14eab6e06a044e91da8f9fb9ade28a2c
twinklose <mathieu.dacheux@icloud.com>
2023-09-25 15:15:07 +0200

    push

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className='border-lime-100 flex-1 justify-center items-center'>
+      <Text>tezljheoiuhgrueoh</Text>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
