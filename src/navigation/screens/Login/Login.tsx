ab0e39087028e4d89e3fa864288c5dedffba1a2c
twinklose <mathieu.dacheux@icloud.com>
2023-09-26 12:00:33 +0200

    Format

--- src/navigation/screens/Login/Login.tsx
+++ src/navigation/screens/Login/Login.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { SafeAreaView, TextInput, Button, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, TextInput, Text, View, Pressable } from 'react-native'
 import { FormikProvider } from 'formik'
 import useLoginFormik from './hooks/useLoginFormik'
 
 export default function Login(): JSX.Element {
   const { loginFormik } = useLoginFormik()
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (!loginFormik.isSubmitting) {
+      setIsSubmitting(false)
+    }
+  }, [loginFormik.isSubmitting])
 
   return (
     <FormikProvider value={loginFormik}>
-      <SafeAreaView>
-        <Text className="w-full text-black text-2xl font-bold font-['Manrope']">
+      <SafeAreaView className='h-[100%] justify-center items-center'>
+        <Text className="w-full h-[50px] text-black text-center text-2xl font-bold font-['SF Pro Text'] justify-center">
           Connexion
         </Text>
-        <Text className="w-full text-black text-lg font-semibold font-['Manrope']">
+        <Text className="w-11/12 text-black text-lg font-semibold font-['SF Pro Text'] mb-2">
           Addresse mail
         </Text>
-        <View className='className="w-11/12 h-[50px] px-[15px] bg-white rounded-[15px] shadow flex justify-center items-center inline-flex"'>
+        <View className='w-11/12 h-[50px] px-[15px] bg-white rounded-[15px] shadow justify-center items-center mb-2'>
           <TextInput
             className="grow shrink basis-0 self-stretch text-gray-500 text-opacity-50 text-sm font-normal font-['SF Pro Text']"
             onChangeText={loginFormik.handleChange('mail')}
@@ -26,10 +33,10 @@ export default function Login(): JSX.Element {
             placeholder='Adresse mail'
           />
         </View>
-        <Text className="w-full text-black text-lg font-semibold font-['Manrope']">
+        <Text className="w-11/12 text-black text-lg font-semibold font-['SF Pro Text'] mb-2">
           Mot de passe
         </Text>
-        <View className='className="w-11/12 h-[50px] px-[15px] bg-white rounded-[15px] shadow flex justify-center items-center inline-flex"'>
+        <View className='w-11/12 h-[50px] px-[15px] bg-white rounded-[15px] shadow flex justify-center items-center mb-3'>
           <TextInput
             className="grow shrink basis-0 self-stretch text-gray-500 text-opacity-50 text-sm font-normal font-['SF Pro Text']"
             onChangeText={loginFormik.handleChange('password')}
@@ -40,11 +47,20 @@ export default function Login(): JSX.Element {
             placeholder='Mot de passe'
           />
         </View>
-        <Button
-          title='Connexion'
+        <Pressable
+          className={`w-[140px] px-[15px] py-[7.5px] rounded-[10px] justify-center items-center shadow
+            ${isSubmitting ? 'bg-indigo-500' : 'bg-indigo-600'}
+          `}
           onPress={() => loginFormik.handleSubmit()}
           disabled={loginFormik.isSubmitting}
-        />
+          onPressIn={() => {
+            setIsSubmitting(true)
+          }}
+        >
+          <Text className="text-white text-lg font-semibold font-['SF Pro Text']">
+            Connexion
+          </Text>
+        </Pressable>
       </SafeAreaView>
     </FormikProvider>
   )
