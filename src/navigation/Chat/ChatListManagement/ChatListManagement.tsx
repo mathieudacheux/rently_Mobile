c8d046c7285c94f0d31f6dff4bf9abd4b94bdec6
twinklose <mathieu.dacheux@icloud.com>
2023-11-21 10:30:53 +0100

    Refactor ChatListManagement component to use dynamic user list

--- src/navigation/Chat/ChatListManagement/ChatListManagement.tsx
+++ src/navigation/Chat/ChatListManagement/ChatListManagement.tsx
@@ -1,20 +1,35 @@
 import { SafeAreaView, View, Text, Image } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import FormikSearchField from '../../../components/molecules/FormikSearchField'
-import Card from '../../../components/atoms/Card'
 import ConversationCard from '../../../components/organisms/ConversationCard'
 
-export default function ChatListManagement(): JSX.Element {
+export default function ChatListManagement({
+  usersList = [],
+  navigateToChat,
+}: {
+  usersList: {
+    id: number
+    name: string
+    lastMessage: string
+    time: string
+    image: string
+  }[]
+  navigateToChat?: (id: number) => Promise<void>
+}): JSX.Element {
   return (
     <SafeAreaView className='w-full items-center'>
       <View className='w-full items-center mt-2'>
         <FormikSearchField title='Rechercher une conversation' />
         <ScrollView className='w-11/12'>
-          <ConversationCard
-            name='Jean Dupont'
-            time='Hier'
-            lastMessage='Bonjour, je suis intéressé par votre bien'
-          />
+          {usersList.map((user) => (
+            <ConversationCard
+              key={user.id}
+              name={user.name}
+              time={user.time}
+              lastMessage={user.lastMessage}
+              onPress={async () => await navigateToChat?.(user.id)}
+            />
+          ))}
         </ScrollView>
       </View>
     </SafeAreaView>
